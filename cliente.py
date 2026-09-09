c6bd7728870eb327510cdec91861cf927ea9a9e2
Stefany Martins <tatiane.s.silva16@aluno.senai.br>
2026-09-09 09:54:16 -0300

    Correção

--- cliente.py
+++ cliente.py
@@ -2,24 +2,12 @@ cliente = {}
 
 nome = input("Digite seu nome: ")
 
-if not nome.isdigit() and nome.replace(" ", ""):
+if not nome:
     print ("Nome inválido")
-    
-telefone = int(input("Digite seu telefone: "))
+    exit()
+else:    
+    telefone = int(input("Digite seu telefone: "))
 
 cliente[nome] = telefone
 
 print("Cliente cadastrado com sucesso!")
-
-
-if nome and telefone in cliente.items():
-    print(f"Nome: {nome}")
-    print(f"Telefone: {telefone}")
-    
-    confirmacao = input("Os dados estão corretos? (S/N): ")
-    
-    if confirmacao.upper() == "S":
-        print("Cadastro confirmado!")
-    elif confirmacao.upper() == "N":
-        print("Cadastro cancelado.")
-        exit()
