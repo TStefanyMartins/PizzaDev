5f536a9924d975d6da7c0e85733624b1970cdbaf
Stefany Martins <tatiane.s.silva16@aluno.senai.br>
2026-09-09 08:04:48 -0300

    Permite escolher uma pizza

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ print("=== PizzaDev ===")
 print("CARDAPIO")
 for indice in range(len(pizzas)):
     print(f"{indice + 1} - {pizzas[indice]}: R$ {precos[indice]:.2f}")
+    
+opcao = input("Escolha o numero da pizza: ")
+if opcao.isdigit() and 1 <= int(opcao) <= len(pizzas):
+    indice = int(opcao) - 1
+    print(f"Pizza escolhida: {pizzas[indice]}")
+    print(f"Preco unitario: R$ {precos[indice]:.2f}")
+else:
+    print("Opcao invalida.")
