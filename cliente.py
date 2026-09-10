1c01db3974642e198cbccfbf471df2a246edccd1
Stefany Martins <tatiane.s.silva16@aluno.senai.br>
2026-09-10 07:59:58 -0300

    Correção2

--- cliente.py
+++ cliente.py
@@ -7,7 +7,11 @@ if not nome:
     exit()
 else:    
     telefone = int(input("Digite seu telefone: "))
+    if len(str(telefone)) != 11:
+        print("Telefone inválido. Deve conter 11 dígitos.")
+        exit()
 
 cliente[nome] = telefone
 
 print("Cliente cadastrado com sucesso!")
+print(cliente)
